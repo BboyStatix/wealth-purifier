deaa641f594fee50ec13f81c584def36b5f725d5
Naveed Elahi <statix231@hotmail.com>
2023-09-24 11:52:47 +0800

    create flask server for generate interest report

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+from services import ExtractInterestService
+
+app = Flask(__name__)
+
+@app.route('/report/generate', methods=['POST'])
+def generate_interest_report():
+  statement_type = request.form.get('statementType')
+  statements = request.files.getlist('files')
+  print("Calculating Interest...")
+  interestEntries = ExtractInterestService.extract_interest(statement_type, statements)
+  totalInterest = sum([entry[1] for entry in interestEntries])
+  print(f"Total Interest: {totalInterest}")
+  return jsonify(interestEntries=interestEntries, total=totalInterest)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=3000, debug=True)
