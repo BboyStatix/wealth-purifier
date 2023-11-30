6b615ab359fa94d022e74798b679fa74b36c0966
Naveed Elahi <statix231@hotmail.com>
2023-11-30 09:10:48 +0800

    support newer hsbc one statements

--- scrapers/HSBCOneInterestScraper.py
+++ scrapers/HSBCOneInterestScraper.py
@@ -3,8 +3,18 @@ import re
 from datetime import datetime
 import io
 
-DATE_REGEX = r"^[0-3]?[0-9] (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)"
-INTEREST_REGEX = "CREDIT INTEREST"
+DATE_REGEX = {
+  "website": r"^[0-3]?[0-9] (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)",
+  "app": r"^[0-3]?[0-9](Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)"
+}
+INTEREST_REGEX_OLD = "CREDIT INTEREST"
+INTEREST_REGEX_NEW = "CREDITINTEREST"
+INTEREST_REGEX = r"({})|({})".format(INTEREST_REGEX_NEW, INTEREST_REGEX_OLD)
+
+STATEMENT_PREFIX_REGEX =  {
+  "website": r"^([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])", # 2021-06-05_Statement
+  "app": r"^eStatementFile_([0-9]{4})(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1]).*" # eStatementFile_20231114192229
+}
 
 def extractPDFContent(pdf):
   pdfContent = ""
@@ -13,29 +23,41 @@ def extractPDFContent(pdf):
   return pdfContent
 
 def getDateFromSentence(sentence):
-  match = re.search(DATE_REGEX, sentence)
+  match = re.search(DATE_REGEX['website'], sentence)
   if match is not None:
     dateStr = match.group(0)
     return datetime.strptime(dateStr, "%d %b")
   
+  match = re.search(DATE_REGEX['app'], sentence)
+  if match is not None:
+    dateStr = match.group(0)
+    return datetime.strptime(dateStr, "%d%b")
+
+def getDateFromStatementFilename(statement):
+  match = re.search(STATEMENT_PREFIX_REGEX["website"], statement)
+  if match is not None:
+    return datetime.strptime(statement.split('_')[0], "%Y-%m-%d")
+  
+  match = re.search(STATEMENT_PREFIX_REGEX["app"], statement)
+  if match is not None:
+    return datetime.strptime(statement.split('_')[1][0:8], "%Y%m%d")
+  
+  raise Exception("Unsupported statement type!")
 
 class HSBCOneInterestScraper:
   def __init__(self, statement, statementData):
-    self.statement = statement
+    self.statementDate = getDateFromStatementFilename(statement)
     with pdfplumber.open(io.BytesIO(statementData)) as pdf:
       self.pdfContent = extractPDFContent(pdf)
 
   def _getFullDateOfCurrentEntry(self, date):
-    isJanuaryStatement = self._getStatementDate().month == 1
+    isJanuaryStatement = self.statementDate.month == 1
     isDecemberEntry = date.month == 12
-    yearOfEntry = self._getStatementDate().year
+    yearOfEntry = self.statementDate.year
     # if we have a January statement, the december entries inside should be currentStatement year - 1
     if isJanuaryStatement and isDecemberEntry:
       yearOfEntry = yearOfEntry - 1
     return f"{date.strftime('%d %b')} {yearOfEntry}"
-
-  def _getStatementDate(self):
-    return datetime.strptime(self.statement.split('_')[0], "%Y-%m-%d")
   
   def _getPdfSentences(self):
     return self.pdfContent.split("\n")
@@ -49,8 +71,13 @@ class HSBCOneInterestScraper:
     raise Exception("No USD Exchange Rate found on PDF")
 
   def _getInterestAmount(self, sentence, isUSD):
-    (_, remainStr) = sentence.split(f"{INTEREST_REGEX} ")
-    amount = float(remainStr.split(" ")[0])
+    # e.g '28Oct CREDITINTEREST 28.08 20,932.88'
+    # e.g '28 Jan CREDIT INTEREST 35.48'
+    pattern = r'(?:{}|{})\s+(\d+(?:\.\d+)?)'.format(INTEREST_REGEX_OLD, INTEREST_REGEX_NEW)
+    match = re.search(pattern, sentence)
+    if match is None: raise Exception("Unsupported file type")
+
+    amount = float(match.group(1))
     if isUSD:
       return amount * self._getUSDExchangeRate()
     else:
@@ -65,7 +92,7 @@ class HSBCOneInterestScraper:
       date = getDateFromSentence(sentence)
       if date: dateOfCurrentEntry = date
       if re.search("Foreign Currency Savings", sentence) is not None: isUSDSection = True
-      if INTEREST_REGEX in sentence:
+      if re.search(INTEREST_REGEX, sentence) is not None:
         fullDate = self._getFullDateOfCurrentEntry(dateOfCurrentEntry)
         interestAmount = self._getInterestAmount(sentence, isUSDSection)
         print(f"{fullDate}: {interestAmount}")
